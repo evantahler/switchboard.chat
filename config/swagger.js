033cdc3b3728a099302f8a81002f74c5d836f49a
Evan Tahler <evantahler@gmail.com>
2016-02-29 10:58:52 -0800

    upgrade

--- config/swagger.js
+++ config/swagger.js
@@ -1,9 +1,11 @@
+var baseUrl = process.env.PUBLIC_URL || 'https://switchboard.chat';
+
 exports['default'] = {
   swagger: function(api){
     return {
       // Should be changed to hit www.yourserver.com.  If this is null, defaults to ip:port from
       // internal values or from hostOverride and portOverride.
-      baseUrl: process.env.PUBLIC_URL.replace('https://', ''),
+      baseUrl: baseUrl.replace('https://', ''),
       // Specify routes that don't need to be displayed
       ignoreRoutes: [ '/swagger' ],
       // Specify how routes are grouped
