a6873e2dadb33cd60209c474fed46f1e9707ccbc
Evan Tahler <evantahler@gmail.com>
2016-06-23 15:57:56 -0700

    upgrade to AH v14.0.0

--- config/redis.js
+++ config/redis.js
@@ -1,3 +1,66 @@
+var host     = process.env.REDIS_HOST || '127.0.0.1';
+var port     = process.env.REDIS_PORT || 6379;
+var database = process.env.REDIS_DB   || 0;
+
+exports['default'] = {
+  redis: function(api){
+    var Redis;
+    if(process.env.FAKEREDIS === 'false' || process.env.REDIS_HOST !== undefined){
+      Redis = require('ioredis');
+    }else{
+      Redis = require('fakeredis');
+    }
+
+    return {
+      '_toExpand': false,
+      // create the redis clients
+      client:     Redis.createClient(port, host, {fast: true}),
+      subscriber: Redis.createClient(port, host, {fast: true}),
+      tasks:      Redis.createClient(port, host, {fast: true}),
+    };
+  }
+};
+
+exports.test = {
+  redis: function(api){
+    if(process.env.FAKEREDIS === 'false'){
+      var Redis = require('ioredis');
+      return {
+        '_toExpand': false,
+
+        client:     new Redis({host: host, port: port, db: database}),
+        subscriber: new Redis({host: host, port: port, db: database}),
+        tasks:      new Redis({host: host, port: port, db: database}),
+      };
+    }else{
+      var Redis = require('fakeredis');
+      return {
+        '_toExpand': false,
+
+        client:     Redis.createClient(port, host, {fast: true}),
+        subscriber: Redis.createClient(port, host, {fast: true}),
+        tasks:      Redis.createClient(port, host, {fast: true}),
+      };
+    }
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 exports.default = {
   redis: function(api){
     var redisDetails = {
