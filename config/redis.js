8b0c4c90a6490339af0046d10264d05e4eb64c07
Evan Tahler <evantahler@gmail.com>
2016-06-24 16:13:07 -0700

    v14.0.2

--- config/redis.js
+++ config/redis.js
@@ -1,108 +1,57 @@
 var host     = process.env.REDIS_HOST || '127.0.0.1';
 var port     = process.env.REDIS_PORT || 6379;
-var database = process.env.REDIS_DB   || 0;
+var db       = process.env.REDIS_DB   || 0;
+var password = process.env.REDIS_PASS || null;
 
 exports['default'] = {
   redis: function(api){
-    var Redis;
+
+    // konstructor: The redis client constructor method
+    // args: The arguments to pass to the constructor
+    // buildNew: is it `new konstructor()` or just `konstructor()`?
+
     if(process.env.FAKEREDIS === 'false' || process.env.REDIS_HOST !== undefined){
-      Redis = require('ioredis');
-    }else{
-      Redis = require('fakeredis');
-    }
 
-    return {
-      '_toExpand': false,
-      // create the redis clients
-      client:     Redis.createClient(port, host, {fast: true}),
-      subscriber: Redis.createClient(port, host, {fast: true}),
-      tasks:      Redis.createClient(port, host, {fast: true}),
-    };
-  }
-};
-
-exports.test = {
-  redis: function(api){
-    if(process.env.FAKEREDIS === 'false'){
-      var Redis = require('ioredis');
       return {
         '_toExpand': false,
-
-        client:     new Redis({host: host, port: port, db: database}),
-        subscriber: new Redis({host: host, port: port, db: database}),
-        tasks:      new Redis({host: host, port: port, db: database}),
+        client: {
+          konstructor: require('ioredis'),
+          args: [{ port: port, host: host, password: password, db: db }],
+          buildNew: true
+        },
+        subscriber: {
+          konstructor: require('ioredis'),
+          args: [{ port: port, host: host, password: password, db: db }],
+          buildNew: true
+        },
+        tasks: {
+          konstructor: require('ioredis'),
+          args: [{ port: port, host: host, password: password, db: db }],
+          buildNew: true
+        }
       };
+
     }else{
-      var Redis = require('fakeredis');
+
       return {
         '_toExpand': false,
-
-        client:     Redis.createClient(port, host, {fast: true}),
-        subscriber: Redis.createClient(port, host, {fast: true}),
-        tasks:      Redis.createClient(port, host, {fast: true}),
+        client: {
+          konstructor: require('fakeredis').createClient,
+          args: [port, host, {fast: true}],
+          buildNew: false
+        },
+        subscriber: {
+          konstructor: require('fakeredis').createClient,
+          args: [port, host, {fast: true}],
+          buildNew: false
+        },
+        tasks: {
+          konstructor: require('fakeredis').createClient,
+          args: [port, host, {fast: true}],
+          buildNew: false
+        }
       };
+
     }
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-exports.default = {
-  redis: function(api){
-    var redisDetails = {
-      // Which channel to use on redis pub/sub for RPC communication
-      channel: 'actionhero',
-      // How long to wait for an RPC call before considering it a failure
-      rpcTimeout: 5000,
-      // which redis package should you ise?
-      pkg: 'ioredis',
-
-      // Basic configuration options
-      host     : process.env.REDIS_HOST || '127.0.0.1',
-      port     : process.env.REDIS_PORT || 6379,
-      database : process.env.REDIS_DB   || 0,
-    };
-
-    if( process.env.FAKEREDIS === 'false' || process.env.REDIS_HOST !== undefined ){
-      redisDetails.pkg  = 'ioredis';
-      // there are many more connection options, including support for cluster and sentinel
-      // learn more @ https://github.com/luin/ioredis
-      redisDetails.options  = {
-        password: (process.env.REDIS_PASS || null),
-      };
-    }
-
-    return redisDetails;
-  }
-};
-
-exports.test = {
-  redis: function(api){
-    var pkg = 'fakeredis';
-    if(process.env.FAKEREDIS === 'false'){
-      pkg = 'ioredis';
-    }
-
-    return {
-      pkg: pkg,
-      host: '127.0.0.1',
-      port: 6379,
-      database: 2,
-      options: {},
-    };
   }
 };
