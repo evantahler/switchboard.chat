6392a43c53961ed119dfdfcc2dbb88778475d066
Evan Tahler <evantahler@gmail.com>
2016-07-01 12:00:31 -0700

    show team error

--- config/api.js
+++ config/api.js
@@ -25,7 +25,9 @@ exports.default = {
       // disables the whitelisting of client params
       disableParamScrubbing: false,
       // params you would like hidden from any logs
-      filteredParams: [],
+      filteredParams: [
+        'password'
+      ],
       // values that signify missing params
       missingParamChecks: [null, '', undefined],
       // The default filetype to server when a user requests a directory
--- public/js/app/controllers/session.js
+++ public/js/app/controllers/session.js
@@ -11,8 +11,14 @@ app.controller('session:create', ['$scope', '$rootScope', '$location', function(
 
   $scope.processForm = function(){
     $rootScope.actionHelper($scope, $scope.formData, '/api/session', 'POST', function(data){
-      if(data.user){ $rootScope.user = data.user; }
-      location.reload(); // <- hack to force the CSRF Token to hydrate
+      if(data.user){
+        $rootScope.user = data.user;
+        location.reload(); // <- hack to force the CSRF Token to hydrate
+      }else if(data.error){
+        ngNotify.set(data.error, 'error');
+      }else{
+        location.reload();
+      }
     });
   };
 }]);
@@ -26,7 +32,7 @@ app.controller('session:destroy', ['$scope', '$rootScope', '$location', function
     $rootScope.actionHelper($scope, {}, '/api/session', 'DELETE', function(data){
       delete $rootScope.user;
       $location.path('/');
-      setTimeout(window.location.reload, 500); // to ensure that any WS connections are terminated. 
+      setTimeout(window.location.reload, 500); // to ensure that any WS connections are terminated.
     });
   };
 }]);
