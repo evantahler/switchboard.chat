091a2bbeb967166dc90476b392f3931052133fcb
Evan <evantahler@gmail.com>
2018-12-09 17:54:11 -0800

    jest testing for next/react

--- web/__tests__/pages/index.js
+++ web/__tests__/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'
-Enzyme.configure({ adapter: new Adapter() })
+import { shallow } from 'enzyme'
+
 import Home from '../../pages/index.js'
 
 describe('Home', () => {
