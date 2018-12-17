86e32b5032bae8ae2173add867581835df65799d
Evan <evantahler@gmail.com>
2018-12-16 16:24:58 -0800

    repositories subscriptions and network calls

--- web/components/layouts/loggedOut.js
+++ web/components/layouts/loggedOut.js
@@ -5,15 +5,13 @@ import Footer from './../footer/footer'
 
 class PageLoggedOut extends React.Component {
   render () {
-    const errorHandler = this.props.errorHandler
-
     return (
       <PageBase>
         <NavbarLoggedOut />
         <br />
         { this.props.children }
         <hr />
-        <Footer/>
+        <Footer />
       </PageBase>
     )
   }
