45aed0a306444990f10a9e6fcac49d791b354419
Evan <evantahler@gmail.com>
2019-02-24 11:53:57 -0800

    layout

--- web/components/cards/whatIsSwitchboard.js
+++ web/components/cards/whatIsSwitchboard.js
@@ -5,16 +5,14 @@ const WhatIsSwitchboard = () => {
     <Card border='info'>
       <Card.Body>
         <Card.Title>What is Switchboard?</Card.Title>
-        <Card.Text>
-          <p>Switchboard.chat is a hosted web application which enables teams to communicate with thier remote employees via SMS. Using a shared, hosted SMS number enables:</p>
-          <ul>
-            <li>Many people to share a single phone number (and without needing to access a physical phone)</li>
-            <li>A mobile-friendly website your team can access whenever they need</li>
-            <li>A shared log of all communications both in and out of the shared number</li>
-            <li>Multiple accounts so you can add and remove team members as your team grows</li>
-            <li>Customizable notifications for unread messages</li>
-          </ul>
-        </Card.Text>
+        <Card.Text>Switchboard.chat is a hosted web application which enables teams to communicate with thier remote employees via SMS. Using a shared, hosted SMS number enables:</Card.Text>
+        <ul>
+          <li>Many people to share a single phone number (and without needing to access a physical phone)</li>
+          <li>A mobile-friendly website your team can access whenever they need</li>
+          <li>A shared log of all communications both in and out of the shared number</li>
+          <li>Multiple accounts so you can add and remove team members as your team grows</li>
+          <li>Customizable notifications for unread messages</li>
+        </ul>
       </Card.Body>
     </Card>
   )
