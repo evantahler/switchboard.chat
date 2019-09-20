4d3512ed35438dd567efbdc2827b983cdaf12299
Evan Tahler <evan@evantahler.com>
2019-09-20 14:21:27 -0700

    industry pages

--- web/pages/about.js
+++ web/pages/about.js
@@ -1,39 +1,40 @@
 import React from 'react'
 import Layout from './../components/layouts/loggedOut.js'
 import { Row, Col } from 'react-bootstrap'
+import JumboImage from '../components/jumboImage'
 
 class Page extends React.Component {
   render () {
     return (
       <Layout>
-        <h1>About</h1>
-        <h4>This application is currently in β (beta)</h4>
+        <h1>About Switchboard</h1>
+        <JumboImage src='/static/images/headers/about.jpg' />
         <Row>
           <Col md={8}>
-            <h2>Who made this?</h2>
+            <h2>Origin Story</h2>
 
             <p>
-              <a href='http://evantahler.com'>Evan</a> made this for his wife Christina, a preschool administrator who was looking for a better way for teachers to communicate unplanned absences and to contact substitute teachers. She dreamed for there to be a way to reach and be reached by teachers (even those without smartphones) that did not necessitate using her personal cell phone. She also wanted a way for other school administrators to be able to view and sometimes help facilitate the notification processes. Subscribing to the mantra of "happy wife happy life", Evan created switchboard.chat to make this dream a reality.
+              <a href='http://evantahler.com'>Evan</a> initially created Switchboard for his wife Christina, while she was workign as a  preschool administrator.  She was looking for a better way for teachers to communicate unplanned absences and to contact substitute teachers. She dreamed for there to be a way to reach teachers (even those without smartphones) that did not necessitate using her personal cell phone. She also wanted a way for other school administrators to be able to view and help facilitate the staffing processes. Subscribing to the mantra of "happy wife happy life", Evan created switchboard.chat to make this dream a reality.
             </p>
 
             <p>
-              There are many industries that still rely on SMS communications between employees, and are doing so with a shared (or personal) phones.  This tool aims to make it easier for the managers of those teams to work with the tool they already use (SMS) but gain some control, centralization, and logging.  Switchboard.chat aims to provide a cheap, sharable alternative to purchasing employee phones.
+              Growing from those beginings, we've learend that there are many industries that still rely on SMS communications between employees, and are doing so by physically sharing phones.  This tool aims to make it easier for the managers of those teams to work with the tool they already use (text-messaging) but gain some control, centralization, and logging.  Switchboard aims to provide a cost-effective, sharable alternative to purchasing employee phones.
             </p>
           </Col>
           <Col md={4}>
-            <h2>Credits</h2>
+            <h2>Thank You</h2>
             <p>Asset Credits:</p>
             <ul>
               <li><a href='https://bootswatch.com'>BootSwatch</a></li>
-              <li><a href='https://thenounproject.com/dergraph/collection/phone'>Phone Icon</a></li>
-              <li><a href='https://thenounproject.com/search/?q=cloud&i=322'>Cloud Icon</a></li>
+              <li><a href='https://www.unsplash.com'>Unsplash</a></li>
             </ul>
 
             <p>Software Credits:</p>
             <ul>
-              <li><a href='http://www.actionherojs.com'>actionhero.js</a></li>
+              <li><a href='http://www.actionherojs.com'>Actionhero</a></li>
               <li><a href='https://reactjs.org/'>React</a></li>
               <li><a href='http://getbootstrap.com'>Bootstrap</a></li>
+              <li><a href='https://react-bootstrap.github.io'>React Bootstrap</a></li>
             </ul>
           </Col>
         </Row>
--- web/pages/industries/education.js
+++ web/pages/industries/education.js
@@ -1,11 +1,61 @@
 import React from 'react'
 import Layout from './../../components/layouts/loggedOut.js'
+import JumboImage from '../../components/jumboImage'
+import Router from 'next/router'
+import { Row, Col, Jumbotron, Button, ListGroup } from 'react-bootstrap'
+import WhatIsSwitchboardCard from '../../components/cards/whatIsSwitchboard'
+import HowDoesSwitchboardWorkCard from '../../components/cards/howDoesSwitchboardWork'
 
 class Page extends React.Component {
   render () {
     return (
       <Layout>
-        <h1>Switchboard for Education</h1>
+        <Row>
+          <Col>
+            <h1>Switchboard for Education</h1>
+            <JumboImage src='/static/images/headers/education.jpg' />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={5}>
+            <h2>What is Switchboard?</h2>
+            <WhatIsSwitchboardCard />
+          </Col>
+
+          <Col md={7}>
+            <h2>How can we help Educators and Administrators?</h2>
+            <p>Switchboard was started to help school administrators contact and schedule subsitute teachers.  From there, we've seen Switchboard be used for many novel purposes:</p>
+            <ListGroup>
+              <ListGroup.Item>Coordinating facilities requests</ListGroup.Item>
+              <ListGroup.Item>Orginizing subsitute teachers</ListGroup.Item>
+              <ListGroup.Item>Emergency communications with parents or caregivers</ListGroup.Item>
+              <ListGroup.Item>Auditing out-of-school communications</ListGroup.Item>
+              <ListGroup.Item>Sharing contact lists for phone chains or activities</ListGroup.Item>
+              <ListGroup.Item>Logging and assigning follow-ups</ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+
+        <br />
+
+        <Row>
+          <Col>
+            <Jumbotron style={{textAlign: 'center'}}>
+              <h4>Join Switchboard today to make running your school even easier!</h4>
+              <br />
+              <Button size='lg' variant='success' onClick={() => Router.push('/session/sign-up')}>Sign Up</Button>
+            </Jumbotron>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={12}>
+            <h2>How does Switchboard Work?</h2>
+            <HowDoesSwitchboardWorkCard />
+          </Col>
+        </Row>
+
       </Layout>
     )
   }
