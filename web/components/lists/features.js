bcbcb1384f9250e493801d4bd15decbb6f834520
Evan Tahler <evan@evantahler.com>
2019-09-20 20:52:55 -0700

    home page images

--- web/components/lists/features.js
+++ web/components/lists/features.js
@@ -1,4 +1,4 @@
-import { Row, Col, Tab, ListGroup } from 'react-bootstrap'
+import { Row, Col, Tab, ListGroup, Image } from 'react-bootstrap'
 
 const FeaturesTable = () => {
   return (
@@ -32,18 +32,23 @@ const FeaturesTable = () => {
             <Tab.Content>
               <Tab.Pane eventKey='#feature1'>
                 <p>See all of your team's messages aggregated in one place! Everyone on the team can see all communications as they happen; in real time!  Keep a record of all incoming and outgoing messages exchanged with each employee.</p>
+                <Image src="/static/images/screenshots/stream.jpg" fluid />
               </Tab.Pane>
               <Tab.Pane eventKey='#feature2'>
                 <p>Optionally configure notifications so that you will never miss a message. You can be notified via SMS or email if too much time has elapsed since your team received a message. You have the ability to set up different notification rules for each team member to create a simple priority tree.</p>
+                <Image src="/static/images/screenshots/notifications.jpg" fluid />
               </Tab.Pane>
               <Tab.Pane eventKey='#feature3'>
                 <p>Need to communicate with other team members?  Leave in-thead messages to coordinate in one place.</p>
+                <Image src="/static/images/screenshots/contact-stream.jpg" fluid />
               </Tab.Pane>
               <Tab.Pane eventKey='#feature4'>
                 <p>Improve your customer service by keeping TODO items alongside your customer communications.  Assign them to your team members and keep track of what is getting done.</p>
+                <Image src="/static/images/screenshots/contact-tasks.jpg" fluid />
               </Tab.Pane>
               <Tab.Pane eventKey='#feature5'>
                 <p>Store both incomming and outgoing message attachments, images, and videos... all included with Switchboard's standard pricing</p>
+                <Image src="/static/images/screenshots/stream.jpg" fluid />
               </Tab.Pane>
               <Tab.Pane eventKey='#feature6'>
                 <p>The Switchboard website and companion iOS app (coming soon) are here to help you at work, and on the go!</p>
