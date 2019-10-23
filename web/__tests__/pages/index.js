29bf21e25aacbcc385d176fecbc80c228dab6699
Evan Tahler <evan@evantahler.com>
2019-10-23 10:12:05 -0700

    fixup test

--- web/__tests__/pages/index.js
+++ web/__tests__/pages/index.js
@@ -6,6 +6,6 @@ import Home from '../../pages/index.js'
 describe('Home', () => {
   it('renders the page', () => {
     const rendered = shallow(<Home />)
-    expect(rendered.html()).toContain('Centralized SMS Communication for Teams')
+    expect(rendered.html()).toContain('Centralized Text Messages for Teams')
   })
 })
